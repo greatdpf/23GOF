989c4e70579acb59a765c99b2034a4fffa860e57
greatdpf <greatdpf@163.com>
2022-04-17 23:47:26 +0800

    实现 Adapter 模式

---
+++ src/com/gof/adapter/AdapterTest.java
@@ -0,0 +1,15 @@
+package com.gof.adapter;
+
+/**
+ * className: AdapterTest
+ * description: 适配器模式
+ * 自我实现适配器模式
+ * author: staring
+ * createDate: 2022/4/17
+ * version: 1.0
+ */
+public class AdapterTest {
+    public static void main(String[] args) {
+
+    }
+}
