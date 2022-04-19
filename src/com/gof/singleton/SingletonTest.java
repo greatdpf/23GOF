bb8416f6c422de7492cec4fc4348557016302974
greatdpf <greatdpf@163.com>
2022-04-20 02:15:34 +0800

    单例模式的饿汉式和懒汉式

--- src/com/gof/singleton/SingletonTest.java
+++ src/com/gof/singleton/SingletonTest.java
@@ -9,22 +9,51 @@ import java.util.Calendar;
  * createDate: 2022/4/19
  * version: 1.0
  */
-public class SingletonTest {
-    private static SingletonTest singletonTest = new SingletonTest();
-    private SingletonTest() {
-        System.out.println("生产一个实例");
-    }
 
-    public static SingletonTest getInstance() {
-        return singletonTest;
+/**
+ * 饿汉式：立即加载，效率高，不能延时加载
+ */
+class HungrySingleton {
+    private static HungrySingleton hungrySingleton = new HungrySingleton();
+    private HungrySingleton() {
+    }
+    public static HungrySingleton getHungrySingleton() {
+        return hungrySingleton;
     }
 }
 
-class Test {
+/**
+ * 懒汉式：延迟加载，由于同步，导致效率较低
+ */
+class LazySingleton {
+    private static LazySingleton lazySingleton;
+    private LazySingleton() {
+    }
+    public static synchronized LazySingleton getLazySingleton() {
+        if (lazySingleton==null) {
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+}
+
+class SingletonTest {
     public static void main(String[] args) {
-        SingletonTest singletonTest1 = SingletonTest.getInstance();
-        System.out.println(singletonTest1);
-        SingletonTest singletonTest2 = SingletonTest.getInstance();
-        System.out.println(singletonTest2);
+        // 测试，如果懒汉式不加同步会发生什么，导致创建的对象不唯一
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                LazySingleton lazySingleton = LazySingleton.getLazySingleton();
+                System.out.println(lazySingleton);
+            }
+        };
+        Thread thread1 = new Thread(runnable);
+        thread1.start();
+        Thread thread2 = new Thread(runnable);
+        thread2.start();
+        Thread thread3 = new Thread(runnable);
+        thread3.start();
+        Thread thread4 = new Thread(runnable);
+        thread4.start();
     }
 }
